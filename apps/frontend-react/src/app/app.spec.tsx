ea716bae3834781236384c3a3dc3b1d7c4c862f5
Nazar Pylypiak <pylypyaknazar@gmail.com>
2025-09-28 14:03:03 +0300

    test: wrap <App> with Redux <Provider> for unit test

--- apps/frontend-react/src/app/app.spec.tsx
+++ apps/frontend-react/src/app/app.spec.tsx
@@ -2,13 +2,17 @@ import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './app';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 describe('App', () => {
   it('should render successfully', () => {
     const { baseElement } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     );
     expect(baseElement).toBeTruthy();
   });
