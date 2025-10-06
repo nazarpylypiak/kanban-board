44bd5ab462ff427f05ab49ad7bbe431890cdc980
Nazar Pylypiak <pylypyaknazar@gmail.com>
2025-10-06 21:03:11 +0300

    refactor: move board management out of dashboard

---
+++ apps/frontend-react/src/app/features/dashboard/containers/Board.tsx
@@ -0,0 +1,50 @@
+import { monitorForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
+import { ITask, TaskStatus } from '@kanban-board/shared';
+import { useEffect, useState } from 'react';
+import Column from '../containers/Column';
+
+const statuses = Object.values(TaskStatus);
+const tasksList: Record<string, ITask[]> = {
+  [TaskStatus.TODO]: [
+    { id: '1', title: 'Design login page', assignedTo: 'Alice' },
+    { id: '2', title: 'Set up database', assignedTo: 'Bob' }
+  ],
+  [TaskStatus.IN_PROGRESS]: [
+    { id: '3', title: 'Create API endpoints', assignedTo: 'Charlie' }
+  ],
+  [TaskStatus.DONE]: [{ id: '4', title: 'Project setup', assignedTo: 'Alice' }]
+};
+
+export default function Board() {
+  const [tasks, setTasks] = useState<Record<TaskStatus, ITask[]>>(tasksList);
+  useEffect(() => {
+    return monitorForElements({
+      onDrop({ source, location }) {
+        const destination = location.current.dropTargets[0];
+
+        if (!destination) return;
+        const destinationLocation = destination.data.col as TaskStatus;
+        const sourceLocation = source.data.col as TaskStatus;
+        if (destinationLocation === sourceLocation) return;
+        const task = source.data.task as ITask;
+
+        setTasks((val) => {
+          return {
+            ...val,
+            [sourceLocation]: val[sourceLocation].filter(
+              ({ id }) => id !== task.id
+            ),
+            [destinationLocation]: [...val[destinationLocation], task]
+          };
+        });
+      }
+    });
+  }, [tasks]);
+  return (
+    <main className="flex gap-4 mt-4 h-screen bg-gray-100">
+      {statuses.map((col: TaskStatus) => (
+        <Column key={col} tasks={tasks} col={col} />
+      ))}
+    </main>
+  );
+}
--- apps/frontend-react/src/app/features/dashboard/pages/DashboardPage.tsx
+++ apps/frontend-react/src/app/features/dashboard/pages/DashboardPage.tsx
@@ -1,54 +1,15 @@
-import { monitorForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
-import { ITask, TaskStatus } from '@kanban-board/shared';
-import { useEffect, useState } from 'react';
+import { IBoard } from '@kanban-board/shared';
+import { useState } from 'react';
+import Board from '../containers/Board';
 import BoardSelector from '../containers/BoardSelector';
-import Column from '../containers/Column';
 
-const statuses = Object.values(TaskStatus);
-const tasksList: Record<string, ITask[]> = {
-  [TaskStatus.TODO]: [
-    { id: '1', title: 'Design login page', assignedTo: 'Alice' },
-    { id: '2', title: 'Set up database', assignedTo: 'Bob' }
-  ],
-  [TaskStatus.IN_PROGRESS]: [
-    { id: '3', title: 'Create API endpoints', assignedTo: 'Charlie' }
-  ],
-  [TaskStatus.DONE]: [{ id: '4', title: 'Project setup', assignedTo: 'Alice' }]
-};
 export default function DashboardPage() {
-  const [tasks, setTasks] = useState<Record<TaskStatus, ITask[]>>(tasksList);
-  useEffect(() => {
-    return monitorForElements({
-      onDrop({ source, location }) {
-        const destination = location.current.dropTargets[0];
+  const [currentBoard, setCurrentBoard] = useState<IBoard | null>(null);
 
-        if (!destination) return;
-        const destinationLocation = destination.data.col as TaskStatus;
-        const sourceLocation = source.data.col as TaskStatus;
-        if (destinationLocation === sourceLocation) return;
-        const task = source.data.task as ITask;
-
-        setTasks((val) => {
-          return {
-            ...val,
-            [sourceLocation]: val[sourceLocation].filter(
-              ({ id }) => id !== task.id
-            ),
-            [destinationLocation]: [...val[destinationLocation], task]
-          };
-        });
-      }
-    });
-  }, [tasks]);
-  const boardSelected = () => {};
   return (
     <>
-      <BoardSelector onSelectBoard={boardSelected} />
-      <main className="flex gap-4 mt-4 h-screen bg-gray-100">
-        {statuses.map((col: TaskStatus) => (
-          <Column key={col} tasks={tasks} col={col} />
-        ))}
-      </main>
+      <BoardSelector onSelectBoard={(board) => setCurrentBoard(board)} />
+      {currentBoard && <Board />}
     </>
   );
 }
